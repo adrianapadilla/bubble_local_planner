ebb5cf5635190a6790776f1e8ea3f791bec78ad0
Adriana Padilla <26433795+adrianapadilla@users.noreply.github.com>
2017-10-17 18:49:59 -0700

    Add files via upload

--- bubble_local_planner/src/bubble_local_planner.cpp
+++ bubble_local_planner/src/bubble_local_planner.cpp
@@ -33,6 +33,7 @@ namespace bubble_local_planner{
 		flag = 1;
 		flag2 = 1;
 		flag3 = 1;
+     		flag4 = 1;
 
 		//initializing speeds
 
@@ -46,10 +47,40 @@ namespace bubble_local_planner{
 		ros::NodeHandle n;
 		amcl_sub = n.subscribe("amcl_pose", 100, &BubblePlannerROS::amclCallback, this);
 		laser_sub = n.subscribe("laser", 100, &BubblePlannerROS::laserCallback, this);
+        	path_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
 
-		//publish to topics
-		//bubble_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+		//initializing the visualization markers
+		points.header.frame_id = "/map";
+	 
+		points.header.stamp = ros::Time::now();
+	 
+		points.ns = "path_drawing";
+	 
+		points.action = visualization_msgs::Marker::ADD;
+	 
+		points.id = 0;
+	 
+		//points.pose.position.x = now.x;
+		//points.pose.position.y = now.y;
+		//points.pose.position.z = 0.5;
+	 
+		points.type = visualization_msgs::Marker::POINTS;
+	 
+		points.scale.x = 0.1;
+		points.scale.y = 0.1;
+	 
+		points.color.g = 1.0f;
+		points.color.a = 1.0;
 
+		average = 0;
+		num = 0;
+		firstTime = 1;
+		hasStarted = 0;
+                pathLength = 0;
+
+		//open file
+		file.open("/home/adriana/adrianaTFG/bubble_cs/bubble_cs_10.txt", ios::out);
+		//file.open("/home/adriana/adrianaTFG/my_robot_name_2dnav/bubble_bc_1.txt", ios::out); //| ios::trunc);
 		// set initialized flag
 		initialized_ = true;
 
@@ -100,10 +131,17 @@ namespace bubble_local_planner{
 		}
 
 		if(length){
+
+			if(firstTime){
+				startTime = ros::Time::now().toSec();
+				firstTime = 0;
+			}
+
+			double beginning = ros::Time::now().toSec();
 			
 			if(!obstacle() & flag){
 
-				ROS_INFO("no obstaclse: normal mode");
+				ROS_INFO("no obstacles: normal mode");
 
 				setNowError(); 
 				
@@ -120,8 +158,23 @@ namespace bubble_local_planner{
 						setNext();
 						
 					}else{
-	
+						stopTime = ros::Time::now().toSec();
 						
+						//time to reach goal
+						ROS_INFO("journey duration: %f", (stopTime-startTime));
+
+						//path length
+						ROS_INFO("path length: %f", pathLength);
+
+						//average executation time (for computational cost)
+						ROS_INFO("avrg exec time: %f", average/num);
+
+						if(file.is_open()){
+							ROS_INFO("I'm OPEN!");
+							//file.close();
+						}else{
+							ROS_INFO("I'm NOT open!");
+						}
 						setVelZ(); 
 						goal_reached_ = true;
 
@@ -146,26 +199,23 @@ namespace bubble_local_planner{
 			}else if(!obstacle() & flag3){
 
 
-				ROS_INFO("no obstacles: avoidance mode");
 
-				if(goalVisible()){  
+				
+				if(goalVisible()){ 
 
-					ROS_INFO("intermediate goal = %d", intermGoal);
-					ROS_INFO("angle= %d", intermGoalA);
-					ROS_INFO("distance = %f", intermGoalD);
-
-					ROS_INFO("yawR fase2 = %f", yawR);
-
-					ROS_INFO("back on the global path!");
-
+					ROS_INFO("goal is visible again!");
+							
 					count = intermGoal;
 					setNext();
-				
-					flag = 1; 
-				
-					 
+
+					ROS_INFO("intermgoal is: %d", intermGoal);
+					
+					flag = 1; //once the goal is visible, we keep following the global path
+					flag2 = 1; //this flag is meant to only allow computeReboundAngle() to go once
+
 				}else{
 
+					ROS_INFO("no obstacles: avoidance mode");
 					computeReboundAngle2();
 					setNowErrorR();
 
@@ -186,31 +236,54 @@ namespace bubble_local_planner{
 
 			}else{
 
-				ROS_INFO("obstacle detected: rebound angle");
 
 				flag = 0;
 				flag3 = 0;
 
-				if(flag2){
-					computeReboundAngle(); 
-					flag2 = 0;
-				}
+				if(goalVisible()){ 
 
-				ROS_INFO("the new angle is: %.4f", yawR);
-				setNowErrorR();
+					ROS_INFO("goal is visible again!");
+							
+					count = intermGoal;
+					setNext();
 
-				if(fabs(nError.az) > 5*D2R){ 
-
-					ROS_INFO("angular: fase 1");
-
-					setRotRR();
+					ROS_INFO("intermgoal is: %d", intermGoal);
 					
+					flag = 1; //once the goal is visible, we keep following the global path
+					flag2 = 1; //this flag is meant to only allow computeReboundAngle() to go once
+
 				}else{
-					flag2 = 1;
-					flag3 = 1;
-				}
+
+
+					ROS_INFO("obstacle detected: rebound angle");
+					
+					if(flag2){
+						computeReboundAngle(); 
+						flag2 = 0;
+					}
+
+					ROS_INFO("the new angle is: %.4f", yawR);
+					setNowErrorR();
+
+					if(fabs(nError.az) > 5*D2R){ 
+
+						ROS_INFO("angular: fase 1");
+
+						setRotRR();
+					
+					}else{
+						flag2 = 1;
+						flag3 = 1;
+					}
 		
+				}
 			}
+
+			//max and min speeds:
+			if(cmd.linear.x>0.3)cmd.linear.x = 0.3;
+			if(cmd.angular.z>0.3)cmd.angular.z = 0.3;
+			if(cmd.linear.x<-0.3)cmd.linear.x = -0.3;
+			if(cmd.angular.z<-0.3)cmd.angular.z = -0.3;
 
 			ROS_INFO("vel->linear.x = %.4f", cmd.linear.x);
 			ROS_INFO("vel->angular.z=%.4f", cmd.angular.z);
@@ -218,11 +291,15 @@ namespace bubble_local_planner{
 			ROS_INFO("count = %d", count);
 			ROS_INFO("length of path: %d", length);
 
+			double ending = ros::Time::now().toSec();
+			average += (ending-beginning);
+			num++;
 
 		}
 
 
 		setBubble();
+		file << cmd.linear.x << " "<< cmd.linear.y << " "<< cmd.angular.z << endl;
 		cmd_vel = cmd;  
 
 		return true;
@@ -253,7 +330,7 @@ namespace bubble_local_planner{
 
 		for(int i = 0; i<180; i++){
 
-			if(laserData.ranges[i]>3) laserData.ranges[i] = 3;
+			if(laserData.ranges[i]>4) laserData.ranges[i] = 4;
 
 		}
  
@@ -264,13 +341,31 @@ namespace bubble_local_planner{
 
 		ROS_INFO("Seq: [%d]", msg->header.seq);
 
-		poseNow.position = msg->pose.pose.position;
-		poseNow.orientation = msg->pose.pose.orientation;
+		//poseNow.position = msg->pose.pose.position;
+		//poseNow.orientation = msg->pose.pose.orientation;
+		
+		pos before;
+		if(hasStarted){
+			before.x = now.x;
+			before.y = now.y;
+		}
+
 		now.x = msg->pose.pose.position.x;
 		now.y = msg->pose.pose.position.y;
 		now.az = getYaw(*msg); 
 		setNowError();
 		setNowErrorR();
+
+		if(hasStarted){
+			pathLength += std::sqrt((now.x-before.x)*(now.x-before.x) + (now.y-before.y)*(now.y-before.y));
+			ROS_INFO("%f, %f, %f", stopTime, startTime, pathLength);
+		}
+
+		//ROS_INFO("%f - %f, %f - %f", now.x, before.x, now.y, before.y);
+
+		pathVisualization();
+		
+		hasStarted = 1;
 
 	}
 
@@ -295,7 +390,7 @@ namespace bubble_local_planner{
 		}
 
 
-		ROS_INFO("bubble radius is %.4f", bubble[0]);
+		ROS_INFO("bubble radius is %.4f", bubble[90]);
 
 				
 		//bubbleVisualization();	
@@ -319,25 +414,56 @@ namespace bubble_local_planner{
 		double den = 0;
 		double zone1 = 0, zone2 = 0, zone3 = 0;
 		int n, N;
+		double ang;
 
 		//check which side has less obstacles:
 
 		//first I add up all the laser readings from each side
 		for(int j = 0; j<90; j++){ 
 
-			if(laserData.ranges[j]<3) {zone1 += laserData.ranges[j];}else{zone1 +=3;}
+			if(laserData.ranges[j]<4) {zone1 += laserData.ranges[j];}else{zone1 +=4;}
 
-			if(laserData.ranges[j+90]<3) {zone3 += laserData.ranges[j+90];}else{zone3 += 3;}
+			if(laserData.ranges[j+90]<4) {zone3 += laserData.ranges[j+90];}else{zone3 += 4;}
 		}
 
 		ROS_INFO("zone1 = %f",zone1);
 		ROS_INFO("zone3 = %f",zone3);
 
+		/*if(zone1>zone3){
+		
+			d = 2; 
+
+			for(int i = 89; i>=0; i--){
+				if((laserData.ranges[i-1]-laserData.ranges[i])>1){
+					ang = i;
+					break;
+				}
+			}
+
+		}else{
+
+			d = 1; 
+
+			for(int i = 90; i<180; i++){
+
+				if((laserData.ranges[i+1]-laserData.ranges[i])>1){
+					ang = i;
+					break;
+				}
+
+			}
+
+		}
+
+
+		yawR = ang*D2R -90*D2R + now.az; 
+		ROS_INFO("division is: %.4f", ang);*/
+
 		//I choose the zone with higher overall laser readings (which means less obstacles):
 		if(zone1>zone3){ 
-			n=0; N=60; d = 2; 
+			n=0; N=90; d = 2; 
 		}else{ 
-		        n=120; N=180; d = 1; 
+		        n=90; N=180; d = 1; 
 		}
 
 		//calculate the weighted arithmetic mean of the zone with less obstacles:
@@ -350,17 +476,138 @@ namespace bubble_local_planner{
 		}
 
 		int k = 0;
-		if(num/den <= 90) k=-1;
-		else k = 1;
+		if(num/den <= 90*D2R) k=1;
+		else k = -1;
 
 		//transforming the angle from the robot's frame to the global frame
-		yawR = (num/den)-90*D2R + k*10*D2R + now.az; 
+		yawR = (num/den)-90*D2R + now.az; //k*10*D2R + now.az; 
 		ROS_INFO("division is: %.4f", (num/den));
 
 
 	}
 
+
 	void BubblePlannerROS::computeReboundAngle2()
+	{
+
+		double thetaObsR, distObsR, thetaObsL, distObsL;
+		double theta, dist, alpha;
+		int angle = 90;
+		double n, p, q;
+		int min, max, m;
+		bool thisWay = 1;
+		double yawR_;
+		bool lala = 0;
+
+
+		theta = 90;
+		dist= laserData.ranges[90];
+
+		if(d == 2){
+			m = -1;  
+			for(int i = 90; i<180 ; i++){
+				if((laserData.ranges[i-1]-laserData.ranges[i])>1){ 
+					
+					if(laserData.ranges[i]<2){
+						theta = i; 
+						dist = laserData.ranges[i];
+						ROS_INFO("lalal1");
+						lala = 1;
+						break; 
+					}
+
+				}
+			}  
+		}
+
+		if(d == 1){
+			m = 1; 
+			for(int i = 90; i>=0 ; i--){
+				if((laserData.ranges[i+1]-laserData.ranges[i])>1){ 
+
+					if(laserData.ranges[i]<2){
+						theta = i; 
+						dist = laserData.ranges[i]; 
+						ROS_INFO("lalal2");
+						lala = 1;
+						break;
+					}
+
+				}
+			}  
+		}
+
+		ROS_INFO("theta = %f", theta);
+
+		/*for(int i = 135; i < 180; i++){
+			if(laserData.ranges[i]<0.8){		
+				close = 1;
+			}
+		}
+
+		for(int i = 0; i < 45; i++){
+			if(laserData.ranges[i]<0.8){		
+				close = 1;
+			}
+		}*/
+		
+		if(dist<=0.8) {
+
+			thetaObsL = 0;
+			distObsL = laserData.ranges[0];
+
+		
+			for(int i = 0; i < 180; i++){
+				if(laserData.ranges[i]<distObsL){		
+					thetaObsL = i;
+					distObsL = laserData.ranges[i];
+				}
+			}
+
+
+			//we will be calculating a weighted arithmetic mean
+
+			p = distObsL/(0.8); //the closer to the obstacle, the more the obstacle's angle weighs
+
+			//q = distObsL/(distObsL + distObsR); //the closer to obstacle L, the more this obstacle's angle weighs
+
+			if(thetaObsL<theta) q = 1;
+			else q = -1;
+		
+			angle = theta*p + (q*180 + thetaObsL)*(1-p);
+
+		
+			ROS_INFO("close to obtacle: %d", angle);
+			
+
+		}else{
+
+			//calculating an angle that will create a distance bigger than 0.8m between the robot and nearby obstacle
+
+			alpha = (1/D2R)*std::asin(0.8/dist);
+			angle = theta + m*alpha;
+
+			ROS_INFO("far from obtacle: %d", angle);
+		
+		}
+
+		if(!lala) angle = theta;
+		// make sure that we chose the smallest angle, so that the robot takes the shortest turn
+		if ( angle > 180 )  angle -= 360;
+		if ( angle < -180 )  angle += 360;
+
+		yawR_ = (angle - 90  + now.az/D2R)*D2R;
+		
+		//trust me, there is a good reason for this:
+		if(fabs(yawR-yawR_)>0.18){
+
+			yawR = yawR_;
+
+		}
+
+	}
+
+/*	void BubblePlannerROS::computeReboundAngle2()
 	{
 
 		double thetaObsR, distObsR, thetaObsL, distObsL;
@@ -370,17 +617,19 @@ namespace bubble_local_planner{
 		int min, max, m;
 		bool thisWay = 1;
 		double yawR_;
+		bool close = 0;
 
 
 		theta = 90;
 		dist= laserData.ranges[90];
 
 		if(d == 2){ 
-			for(int i = 90; i<179 ; i++){
+			for(int i = 87; i<180 ; i++){
 				if((laserData.ranges[i-1]-laserData.ranges[i])>1){ 
 					
 						theta = i; 
 						dist = laserData.ranges[i];
+						ROS_INFO("lalal1");
 						m = -1;
 						break; 
 
@@ -389,11 +638,12 @@ namespace bubble_local_planner{
 		}
 
 		if(d == 1){
-			for(int i = 90; i>=0 ; i--){
+			for(int i = 93; i>=0 ; i--){
 				if((laserData.ranges[i+1]-laserData.ranges[i])>1){ 
 
 						theta = i; 
 						dist = laserData.ranges[i]; 
+						ROS_INFO("lalal2");
 						m = 1; 
 						break;
 
@@ -401,39 +651,44 @@ namespace bubble_local_planner{
 			}  
 		}
 
+		ROS_INFO("thetaDisc = %f", theta);
+
+		/*for(int i = 135; i < 180; i++){
+			if(laserData.ranges[i]<0.8){		
+				close = 1;
+			}
+		}
+
+		for(int i = 0; i < 45; i++){
+			if(laserData.ranges[i]<0.8){		
+				close = 1;
+			}
+		}/*****
 
 		if(dist<=0.8) {
 
 			thetaObsL = 0;
 			distObsL = laserData.ranges[0];
 
-			thetaObsR = theta;
-			distObsR = laserData.ranges[90]; 
-
 		
-			for(int i = 0; i < theta; i++){
+			for(int i = 0; i < 180; i++){
 				if(laserData.ranges[i]<distObsL){		
 					thetaObsL = i;
 					distObsL = laserData.ranges[i];
 				}
 			}
 
-			for(int i = theta; i < 180; i++){
-				if(laserData.ranges[i]<distObsR){		
-					thetaObsR = i;
-					distObsR = laserData.ranges[i];
-				}
-			}
 
 			//we will be calculating a weighted arithmetic mean
 
-			n = distObsR/(distObsR + 0.5); //the closer to the obstacle, the more the obstacle's angle weighs
+			p = distObsL/(0.8); //the closer to the obstacle, the more the obstacle's angle weighs
 
-			p = distObsL/(distObsL + 0.5); //the closer to the obstacle, the more the obstacle's angle weighs
+			//q = distObsL/(distObsL + distObsR); //the closer to obstacle L, the more this obstacle's angle weighs
 
-			q = distObsL/(distObsL + distObsR); //the closer to obstacle L, the more this obstacle's angle weighs
+			if(thetaObsL<theta) q = 1;
+			else q = -1;
 		
-			angle = q*(theta*n + (-180 + thetaObsR)*(1-n)) + (1-q)*(theta*p + (180 + thetaObsL)*(1-p));
+			angle = theta*p + (q*180 + thetaObsL)*(1-p);
 
 		
 			ROS_INFO("close to obtacle");
@@ -443,7 +698,7 @@ namespace bubble_local_planner{
 
 			//calculating an angle that will create a distance bigger than 0.8m between the robot and nearby obstacle
 
-			alpha = (1/D2R)*std::asin(0.8/dist);
+			alpha = (1/D2R)*std::asin(0.5/dist);
 			angle = theta + m*alpha;
 
 			ROS_INFO("far from obtacle");
@@ -463,7 +718,7 @@ namespace bubble_local_planner{
 
 		}
 
-	}
+	}*/
 
 	void BubblePlannerROS::setNext()
 	{
@@ -496,7 +751,7 @@ namespace bubble_local_planner{
 	void BubblePlannerROS::setNowError()
 	{
 
-		double d;
+		double ang;
 
 		nError.x = (next.x - now.x);
 		nError.y = (next.y - now.y);
@@ -505,13 +760,13 @@ namespace bubble_local_planner{
 		// plus, the angle error is irrelevant because we have arrived at our destination
 
 		if (nError.y == 0 & nError.x == 0){  
-			d = now.az;
+			ang = now.az;
 		}else{	
-			d = std::atan2(nError.y, nError.x);
+			ang = std::atan2(nError.y, nError.x);
 		}
 
 		distance = std::sqrt(nError.x*nError.x +nError.y*nError.y);
-		nError.az = d - now.az;
+		nError.az = ang - now.az;
 
 		// make sure that we chose the smallest angle, so that the robot takes the shortest turn
 		if ( nError.az > 180*D2R ) { nError.az -= 360*D2R; }
@@ -653,7 +908,7 @@ namespace bubble_local_planner{
 	{
 
 		pos iG;
-		double d;
+		double ang;
 
 		iG.x = plan[intermGoal].pose.position.x - now.x;
 		iG.y = plan[intermGoal].pose.position.y - now.y;
@@ -662,13 +917,13 @@ namespace bubble_local_planner{
 		ROS_INFO("distance y = %f", iG.y);
 
 		if (iG.y == 0 & iG.x == 0){  
-			d = now.az;
+			ang = now.az;
 		}else{	
-			d = std::atan2(iG.y, iG.x); 
+			ang = std::atan2(iG.y, iG.x); 
 		}
 
 		intermGoalD = std::sqrt(iG.x*iG.x + iG.y*iG.y);
-		intermGoalA = d/D2R;
+		intermGoalA = ang/D2R;
 
 		// make sure that we chose the smallest angle, so that the robot takes the shortest turn
 		if ( intermGoalA > 180 )  intermGoalA -= 360;
@@ -677,61 +932,161 @@ namespace bubble_local_planner{
 	}
 
 
-
 	int BubblePlannerROS::findIntermGoal()
 	{
 		int nextPoint = 0;
+		int minimum;
+		int min, max;
 
-		for(int m = length-1; m > count+100; m--){ 
+		if(count+100>=(length-1)) minimum = length-2;
+		else minimum = count + 90; 
+		
+		for(int k = length-1; k > minimum; k--){  
+		//for(int k = length-1; k > count; k--){  
 				
 				int allClear = 1;
 				double alpha;	
 				double dist;	
-				pos fError;
-				int min, max;
+				pos iError;
 
-				fError.x = plan[m].pose.position.x - now.x;
-				fError.y = plan[m].pose.position.y - now.y;
+				iError.x = plan[k].pose.position.x - now.x;
+				iError.y = plan[k].pose.position.y - now.y;
 
-				dist = std::sqrt(fError.x*fError.x +fError.y*fError.y);		
+				dist = std::sqrt(iError.x*iError.x +iError.y*iError.y);
+				
+				//alpha = std::atan2(iError.y,iError.x); //this is the angle to the goal (straight line)
 
-				alpha = std::atan2(fError.y,fError.x); //this is the angle to the goal (straight line)
+				//alpha = alpha +90*D2R -now.az; // passing to robot's frame
 
-				alpha = alpha +90*D2R -now.az; // passing to robot's frame
+				//alpha = alpha/D2R; //passing radians to degrees
 
-				alpha = alpha/D2R; //passing radians to degrees
+				//if(alpha < 0 | alpha >= 180) return 0;
 
-				if(dist>1){
+				//if((dist<=1)&(laserData.ranges[alpha] >= dist)) nextPoint = m;
 
+				if(dist>3){
 					allClear = 0;
 				}else{
 
-					if(alpha < 154 | alpha > 25){
+					alpha = std::atan2(iError.y,iError.x); //this is the angle to the goal 
 
-						min = alpha - 25;
-						max = alpha + 25;
+					//ROS_INFO("alpha %f", alpha/D2R);
 
+					alpha = alpha +90*D2R -now.az; // passing to robot's frame
+					alpha = alpha/D2R; //passing radians to degrees
+					if ( alpha > 180 ) { alpha -= 360; }
+					if ( alpha < -180 ) { alpha += 360;  }
+
+					//ROS_INFO("now.az %f", now.az/D2R);
+
+					min = alpha - 20;
+					max = alpha + 20;
+
+					/*ROS_INFO("alpha %f", alpha);
+					ROS_INFO("min %d", min);
+					ROS_INFO("max %d", max);
+					ROS_INFO("error x %f", iError.x);
+					ROS_INFO("error y %f", iError.y);*/
+
+					/*if(min < 0) min = 0;
+					if(max > 180) max = 180;
+
+					ROS_INFO("alpha %f", alpha);
+					ROS_INFO("min %d", min);
+					ROS_INFO("max %d", max);
+					ROS_INFO("error x %f", iError.x);
+					ROS_INFO("error y %f", iError.y);
+
+					ROS_INFO("point %d", k);
+					ROS_INFO("angle %f", alpha);
+					for(int i = min; i < max; i++)
+					{
+						if(laserData.ranges[i] < 2) {
+							allClear = 0; 
+							ROS_INFO("laser[%d] = %f", i, laserData.ranges[i]);
+						}
+					}*/
+					if(min < 0 | max > 180) {
+						allClear = 0;
+						//ROS_INFO("point %d", k);
+					}else{
+						//ROS_INFO("point %d", k);
+						//ROS_INFO("angle %f", alpha);
 						for(int i = min; i < max; i++)
 						{
-
-							if(laserData.ranges[i] < 1) allClear = 0; 
-
+							if(laserData.ranges[i] < 2) {
+								allClear = 0; 
+								//ROS_INFO("laser[%d] = %f", i, laserData.ranges[i]);
+							}
 						}
-
-						if (allClear) nextPoint = m;
-
 					}
 
 				}
-				
+
+				if (allClear == 1){ 
+					nextPoint = k;
+					/*ROS_INFO("alpha = %f", alpha);
+					ROS_INFO("iError.x = %f",iError.x);
+					ROS_INFO("iError.y = %f", iError.y);
+					ROS_INFO("dist = %f", dist);
+					ROS_INFO("minimum = %d", minimum);
+					for(int i = min; i < max; i++)
+					{
+						ROS_INFO("laser[%d] = %f", i, laserData.ranges[i]);
+					}*/
+				}
 			
 		}
 
+		ROS_INFO("intermediate goal is:=%d", nextPoint);
 
 		return nextPoint;
 	}
 
 
-	
+	void BubblePlannerROS::pathVisualization()
+	{
+		// Visualization in rviz
+	 
+		/*visualization_msgs::Marker points;
+	 
+		points.header.frame_id = "/map";
+	 
+		points.header.stamp = ros::Time::now();
+	 
+		points.ns = "path_drawing";
+	 
+		points.action = visualization_msgs::Marker::ADD;
+	 
+		points.id = 0;
+	 
+		//points.pose.position.x = now.x;
+		//points.pose.position.y = now.y;
+		//points.pose.position.z = 0.5;
+	 
+		points.type = visualization_msgs::Marker::POINTS;
+	 
+		points.scale.x = 0.1;
+		points.scale.y = 0.1;
+	 
+		points.color.g = 1.0f;
+		points.color.a = 1.0;*/
+	 
+	        geometry_msgs::Point p;
+ 
+	        p.x = now.x;
+	        p.y = now.y;
+	        p.z = 0.5;
+ 
+      	        points.points.push_back(p);
+   
+	     
+		path_pub.publish(points); 
+	 
+	 }	
 
 }
+
+
+
+
